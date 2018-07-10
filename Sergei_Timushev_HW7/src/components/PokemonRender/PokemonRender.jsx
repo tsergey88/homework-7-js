712499987810726c956f0ccd8efd94c09c492d47
Sergei Timushev <tsergeyrol08@rambler.ru>
2018-07-10 15:00:33 +0300

    Update PokemonRender.jsx

--- Sergei_Timushev_HW7/src/components/PokemonRender/PokemonRender.jsx
+++ Sergei_Timushev_HW7/src/components/PokemonRender/PokemonRender.jsx
@@ -8,26 +8,26 @@ import Col  from 'react-bootstrap/lib/Col';
 
 export default class PokemonRender extends Component {
     render() {
-		const { name, id, isCaptured, addToCaptured} = this.props;
+		  const { name, id, isCaptured, addToCaptured} = this.props;
 
-        return (
-			<Col xs={12} md={6} lg={4} className="text-center item">
-				<div>
-					<Link to={`/pokemon/${id}`} className="text-center">
-						<img src={`https://github.com/epam-js-may-2018/homework-7-js/raw/master/pokemons/${id}.png`} alt={name} className="img-responsive" />
-					</Link>
-				</div>
-				<div>
-					<Link to={`/pokemon/${id}`}>
-						<h2>{name}</h2>
-					</Link>
-				</div>
-				<div>
-					<Button onClick={addToCaptured} disabled={isCaptured}>
-						{isCaptured ? 'Caught' : 'Catch'}
-					</Button>
-				</div>
-			</Col>
-        );
+      return (
+        <Col xs={12} md={6} lg={4} className="text-center item">
+          <div>
+            <Link to={`/pokemon/${id}`} className="text-center">
+              <img src={`https://github.com/epam-js-may-2018/homework-7-js/raw/master/pokemons/${id}.png`} alt={name} className="img-responsive" />
+            </Link>
+          </div>
+          <div>
+            <Link to={`/pokemon/${id}`}>
+              <h2>{name}</h2>
+            </Link>
+          </div>
+          <div>
+            <Button onClick={addToCaptured} disabled={isCaptured}>
+              {isCaptured ? 'Caught' : 'Catch'}
+            </Button>
+          </div>
+        </Col>
+      );
     }
 }
