e444587a4430be86e00b05018cb314761a3a7667
Sergei Timushev <tsergeyrol08@rambler.ru>
2018-07-10 14:57:39 +0300

    Update PokemonPageRender.jsx
    tabs

--- Sergei_Timushev_HW7/src/components/PokemonPageRender/PokemonPageRender.jsx
+++ Sergei_Timushev_HW7/src/components/PokemonPageRender/PokemonPageRender.jsx
@@ -12,7 +12,7 @@ export default class PokemonPageRender extends Component {
 	render() {
 		const { name, id, status, dateCaptured, captured } = this.props;
 		return (
-            <Grid className="item-block">
+      <Grid className="item-block">
 				<Row className="show-grid">
 					<Col xs={12} md={6} lg={6} mdOffset={3} lgOffset={3} className="text-center">
 					<img src={`https://github.com/epam-js-may-2018/homework-7-js/raw/master/pokemons/${id}.png`} alt={name} />
@@ -27,4 +27,4 @@ export default class PokemonPageRender extends Component {
 			</Grid>
 		);
 	}
-}
+}tabs
