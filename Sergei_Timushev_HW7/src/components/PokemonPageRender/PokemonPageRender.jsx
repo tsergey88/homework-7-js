4b936941e514ab800d4ed80fce2f872556d335bb
Sergei Timushev <tsergeyrol08@gmail.com>
2018-07-10 15:28:59 +0300

    TABSSSS

--- Sergei_Timushev_HW7/src/components/PokemonPageRender/PokemonPageRender.jsx
+++ Sergei_Timushev_HW7/src/components/PokemonPageRender/PokemonPageRender.jsx
@@ -10,21 +10,21 @@ import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
 
 export default class PokemonPageRender extends Component {
 	render() {
-		const { name, id, status, dateCaptured, captured } = this.props;
-		return (
+	  const { name, id, status, dateCaptured, captured } = this.props;
+	  return (
       <Grid className="item-block">
-				<Row className="show-grid">
+			  <Row className="show-grid">
 					<Col xs={12} md={6} lg={6} mdOffset={3} lgOffset={3} className="text-center">
-					<img src={`https://github.com/epam-js-may-2018/homework-7-js/raw/master/pokemons/${id}.png`} alt={name} />
-						<ListGroup>
+				    <img src={`https://github.com/epam-js-may-2018/homework-7-js/raw/master/pokemons/${id}.png`} alt={name} />
+					  <ListGroup>
 						  <ListGroupItem bsStyle="success">ID: {id}</ListGroupItem>
-							<ListGroupItem bsStyle="info">NAME: {name}</ListGroupItem>
-							<ListGroupItem bsStyle="warning">STATUS: {status}</ListGroupItem>
-							<ListGroupItem bsStyle="danger">{captured ? 'DATE CAPTURED: ' : ''}{dateCaptured}</ListGroupItem>
-						</ListGroup>
-					</Col>
-				</Row>
-			</Grid>
+						  <ListGroupItem bsStyle="info">NAME: {name}</ListGroupItem>
+						  <ListGroupItem bsStyle="warning">STATUS: {status}</ListGroupItem>
+						  <ListGroupItem bsStyle="danger">{captured ? 'DATE CAPTURED: ' : ''}{dateCaptured}</ListGroupItem>
+					  </ListGroup>
+				  </Col>
+			  </Row>
+		  </Grid>
 		);
 	}
 }
