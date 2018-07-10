c89cb54f132885a8045c7f4761f5e1bf38caaf44
Sergei Timushev <tsergeyrol08@rambler.ru>
2018-07-10 14:56:54 +0300

    Update Header.jsx
    tabs

--- Sergei_Timushev_HW7/src/components/Header/Header.jsx
+++ Sergei_Timushev_HW7/src/components/Header/Header.jsx
@@ -5,20 +5,20 @@ import Navbar from 'react-bootstrap/lib/Navbar';
 export default class Header extends Component {
 	render() {
 		return (
-            <Navbar inverse collapseOnSelect fixedTop>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to="/">Pokedex</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <ul className="nav nav-pills navbar-nav">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/captured-pokemons">Captured Pokemons</Link></li>
-                    </ul>
-                </Navbar.Collapse>
-            </Navbar>
+      <Navbar inverse collapseOnSelect fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Pokedex</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <ul className="nav nav-pills navbar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/captured-pokemons">Captured Pokemons</Link></li>
+          </ul>
+        </Navbar.Collapse>
+      </Navbar>
 		);
 	}
 }
